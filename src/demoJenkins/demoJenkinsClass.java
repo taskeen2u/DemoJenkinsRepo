d17a7a4dea4a50b6902bb4a1099d39395fa4adf1
FATIMAT <FATIMAT@ATNSW-BENCH012.dialoggroup.internal>
2016-05-10 11:53:07 +1000

    123

--- src/demoJenkins/demoJenkinsClass.java
+++ src/demoJenkins/demoJenkinsClass.java
@@ -9,6 +9,8 @@ public class demoJenkinsClass {
 	@Test
 	public void testJenkins(){
 		
+		System.out.println("Jenkins");
+		System.out.println("Welcome");
 		System.out.println("Welcome to Jenkins world");
 		System.out.println("New script line");
 		WebDriver driver= new FirefoxDriver();
